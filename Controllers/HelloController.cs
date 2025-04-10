18bc70a31dfb06adae5358e3c007f7965ff40661
Parshvi018 <parshvi.k@somaiya.edu>
2025-04-10 18:38:47 +0530

    Initial commit

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC1.Models;
+
+namespace MVC1.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            TempData["Location"] = "India";
+            return View();
+        }
+        public IActionResult Student()
+        {
+            Student student = new Student
+            {
+                Id = 1,
+                Name = "Test",
+                Gender = "Female"
+            };
+            ViewBag.CurrentDate = DateTime.Now.ToShortDateString();
+            ViewData["Title"] = "Miss";
+            string? Location= Convert.ToString(TempData["Location"]);
+            return View(student);
+
+        }
+    }
+}
